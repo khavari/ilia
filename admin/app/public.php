254516c74b3d222d24ebc01cdd6ea64e4818e537
Mohammad Khavari <khavari.mvc@gmail.com>
2019-01-10 19:39:31 +0330

    Implement success and error flash message

--- admin/app/public.php
+++ admin/app/public.php
@@ -32,3 +32,58 @@ function current_url()
     return $_SERVER['PHP_SELF'] . $query_string;
 }
 
+
+/**
+ * Set session for flash message
+ * success & success_time
+ * error & error_time
+ *
+ * @param $type
+ * @param $message
+ */
+function set_message($type, $message)
+{
+    $_SESSION[$type] = $message;
+    $_SESSION[$type . '_time'] = time();
+}
+
+/**
+ * Return the specified session if is set.
+ * success or error
+ *
+ * @param $type
+ *
+ * @return null
+ */
+function get_message($type)
+{
+    if (isset($_SESSION[$type])) {
+        return $_SESSION[$type];
+    }
+
+    return null;
+}
+
+/**
+ * Check the specified session is set or no.
+ * Unset the specified session if is expired.
+ *
+ * @param $type
+ *
+ * @return bool
+ */
+function has_message($type)
+{
+    if (isset($_SESSION[$type], $_SESSION[$type . '_time'])) {
+        $old_time = $_SESSION[$type . '_time'];
+        if ((time() - $old_time) < 3) {
+            return true;
+        } else {
+            unset($_SESSION[$type], $_SESSION[$type . '_time']);
+        }
+
+        return false;
+    }
+
+    return false;
+}
--- admin/master/script.php
+++ admin/master/script.php
@@ -4,3 +4,21 @@
 <script src="./assets/js/jquery.slimscroll.min.js"></script>
 <script src="./assets/js/adminlte.min.js"></script>
 <script src="./assets/js/demo.js"></script>
+<script src="./assets/js/sweetalert.min.js"></script>
+<?php if (has_message('success')): ?>
+    <script>
+      swal({
+        icon: 'success',
+        text: "<?=get_message('success');?>",
+        button: false,
+      })
+    </script>
+<?php elseif (has_message('error')): ?>
+    <script>
+      swal({
+        icon: 'error',
+        text: "<?=get_message('error');?>",
+        button: false,
+      })
+    </script>
+<?php endif; ?>
